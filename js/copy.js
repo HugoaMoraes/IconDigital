91b745c1606ff04ef73797056aecdea87dbfce1d
Hugo Moraes <102623594+HugoaMoraes@users.noreply.github.com>
2023-10-18 13:45:23 -0300

    v.2.2

---
+++ js/copy.js
@@ -0,0 +1,33 @@
+function copiarTexto(element) {
+    var email = element.getAttribute('value');
+
+    var el = document.createElement('textarea');
+    el.value = email;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+
+    var selecionado =
+        document.getSelection().rangeCount > 0
+            ? document.getSelection().getRangeAt(0)
+            : false;
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    if (selecionado) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selecionado);
+    }
+
+    var copiado = document.createElement('div');
+    copiado.className = 'copiado-popup';
+    copiado.textContent = 'E-mail Copiado';
+
+    document.body.appendChild(copiado);
+
+    setTimeout(function () {
+        copiado.remove();
+    }, 2000);
+}
